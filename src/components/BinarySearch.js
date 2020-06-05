332ff5d26e4bee38922aa50e407c84e95ea7eed8
Arjun <arj.kashyap123@gmail.com>
2020-06-06 02:29:45 +0530

    Wrote bin search using interval method

--- src/components/BinarySearch.js
+++ src/components/BinarySearch.js
@@ -27,6 +27,9 @@ const BinarySearch = () => {
   const [found, setFound] = useState(-1);
   const [msg, setMsg] = useState("");
 
+  // Window for rendering annimation color
+  const [win, setWin] = useState({ l: -1, m: -1, r: -1 });
+
   useEffect(() => {
     if (searching) setMsg(`Searching for ${searchElement}`);
     else setMsg(`Search for ${searchElement}`);
@@ -40,63 +43,57 @@ const BinarySearch = () => {
         <p>{e}</p>
       </div>
     ));
+    console.log(win);
     setComponent(Array);
-  }, [ArrData.Array, searching, searchElement]);
-
-  // Pull out number from array of divs
-  const getNumber = (i) => parseInt(component[i].props.children.props.children);
+  }, [ArrData.Array, searching, searchElement, win]);
 
   // Returns color for a particular array ind
 
   // Start binary search annimation
   const binSearch = () => {
+    if (searching) return;
+
+    setSearching(true);
     console.log("started");
     let arr = ArrData.Array;
     // format arr
     arr = arr.map((e) => parseInt(e, 10));
     const search = parseInt(ArrData.Search, 10);
     console.log(arr);
+    console.log(search);
     let start = 0;
     let end = arr.length - 1;
     let mid;
-
+    setWin({ ...win, l: start, r: end });
     let binSearch = setInterval(() => {
       if (start > end) {
         setFound(-1);
+        setMsg(`Element not found in the array. . .`);
         clearInterval(binSearch);
       }
-
+      // console.table(win);
       mid = parseInt((start + end) / 2, 10);
+      setWin({ ...win, l: start, m: mid, r: end });
       console.log("start ", start);
       console.log("mid ", mid);
-      console.log("end",  end);
+      console.log("end", end);
       if (search === arr[mid]) {
         console.log("fouuunndd");
         setFound(mid);
+        setMsg(`Element found at index: ${mid}`);
         console.log(mid);
+        clearInterval(binSearch);
         return;
       } else if (search > arr[mid]) {
+        // setWin({ ...win, l: start });
+
         start = mid + 1;
       } else if (search < arr[mid]) {
         end = mid - 1;
+        // setWin({ ...win, r: end });
       }
     }, 1000);
-    // while (start <= end) {
-    //   mid = parseInt((start + end) / 2, 10);
-    //   console.log(start);
-    //   console.log(mid);
-    //   console.log(end);
-    //   if (search === arr[mid]) {
-    //     console.log("fouuunndd");
-    //     setFound(mid);
-    //     console.log(mid);
-    //     return;
-    //   } else if (search > arr[mid]) {
-    //     start = mid + 1;
-    //   } else if (search < arr[mid]) {
-    //     end = mid - 1;
-    //   }
-    // }
+
     setFound(-1);
   };
 
@@ -113,7 +110,6 @@ const BinarySearch = () => {
         <button id="search-btn" className="button" onClick={binSearch}>
           Search : {searchElement}
         </button>
-        <p>{found}</p>
       </div>
     </div>
   );
